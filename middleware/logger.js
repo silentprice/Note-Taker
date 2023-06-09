0f5ae2c51f6aa981816b53c4db384a1b360d1e60
Nick <nprice0502@yahoo.com>
2023-06-09 18:58:42 -0400

    created middleware folder/file and wrote basic function

---
+++ middleware/logger.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+// Middleware function to log request information
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+};
+
+// Use the logger middleware for all requests
+app.use(logger);
+
+// Route handler for GET requests to the root path
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(3001, () => {
+  console.log('Server listening on port 3001');
+});
